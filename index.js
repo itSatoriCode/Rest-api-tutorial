4663a730ffa163f82aafcdbc93fd13d09da96b7d
MrGeet <aldarbatomunkuev@gmail.com>
2021-04-08 12:28:14 +1200

    Using morgan, moving listen to bottom and adding error handlers

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
+const morgan = require('morgan');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.listen(port, console.log('Server is running on the port 5000'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-	// res.send('Hello there my dear friends');
 	res.json({
-		message: 'Obi wan kenobi',
+		message: 'Welcome to itSatori API',
 	});
 });
 
 app.use('/products', productRoutes);
-
 app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+	const err = new Error('Not found');
+	err.status(404);
+	next();
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: 'Not found',
+		},
+	});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log('Server is running on the port 5000'));
